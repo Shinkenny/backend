825791db43ba4faba5fbd9a0432c64f6ad71be15
Sebastien Corbelin <corbatello@gmail.com>
2021-01-24 15:13:55 +0100

    Finalisation du code

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -6,7 +6,9 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
   });
   sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
@@ -39,7 +41,9 @@ exports.deleteSauce = (req, res, next) => {
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
+    .then(sauce => {
+    	res.status(200).json(sauce)
+    })
     .catch(error => res.status(404).json({ error }));
 };
 
@@ -48,3 +52,50 @@ exports.getAllSauces = (req, res, next) => {
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.updateLikes = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            switch (req.body.like) {
+                case -1:
+                    sauce.dislikes = sauce.dislikes + 1;
+                    sauce.usersDisliked.push(req.body.userId);
+                    sauceObject = {
+                        "dislikes": sauce.dislikes,
+                        "usersDisliked": sauce.usersDisliked
+                    }
+                    break;
+                case 0:
+                    if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+                        sauce.usersDisliked = sauce.usersDisliked.filter(user => user !== req.body.userId);
+                        sauce.dislikes = sauce.dislikes - 1;
+                        sauceObject = {
+                            "dislikes": sauce.dislikes,
+                            "usersDisliked": sauce.usersDisliked
+                        }
+                    } else {
+                        sauce.usersLiked = sauce.usersLiked.filter(user => user !== req.body.userId);
+                        sauce.likes = sauce.likes - 1;
+                        sauceObject = {
+                            "likes": sauce.likes,
+                            "usersLiked": sauce.usersLiked
+                        }
+                    }
+                    break;
+                case +1:
+                    sauce.likes = sauce.likes + 1;
+                    sauce.usersLiked.push(req.body.userId);
+                    sauceObject = {
+                        "likes": sauce.likes,
+                        "usersLiked": sauce.usersLiked
+                    }
+                    break;
+                default:
+                    return res.status(500).json({ error });
+            }
+            Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Avis donné !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(() => res.status(400).json({ error: 'Sauce non trouvée !' }));
+}
--- routes/sauce.js
+++ routes/sauce.js
@@ -6,6 +6,7 @@ const multer = require('../middleware/multer-config');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/:id/like', auth, sauceCtrl.updateLikes);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
